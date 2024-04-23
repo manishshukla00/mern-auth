b915a8614ae6aee3ceb4a6808007f8fe50a43707
manishshukla00 <manishshuklacreative@outlook.com>
2024-04-23 18:25:57 +0530

    create signin api route

--- api/controllers/authControllers.js
+++ api/controllers/authControllers.js
@@ -1,6 +1,7 @@
 import User from "../models/userModel.js";
 import bcryptjs from "bcryptjs";
 import { errorHandler } from "../utils/error.js";
+import jwt from "jsonwebtoken";
 
 export const signup = async (req, res, next) => {
   const { username, email, password } = req.body;
@@ -16,6 +17,32 @@ export const signup = async (req, res, next) => {
       .status(200)
       .json({ success: true, message: "User Created Successfully" });
   } catch (error) {
+    next(error);
+  }
+};
+
+export const signin = async (req, res, next) => {
+  const { email, password } = req.body;
+  try {
+    const user = await User.findOne({ email });
+    if (!user) {
+      return next(errorHandler(404, "User Not Found"));
+    }
+    const comparePassword = bcryptjs.compareSync(password, user.password);
+    if (!comparePassword) {
+      return next(errorHandler(401, "Wrong Credential"));
+    }
+    const token = jwt.sign({ id: user._id }, process.env.JWT_SECRET);
+    const expiryDate = new Date(Date.now() + 3600000);
+    res.cookie("access_token", token, { httpOnly: true, expires: expiryDate });
+    return res.status(200).json({
+      success: true,
+      message: "User Signin Successfully",
+      user,
+      token,
+    });
+  } catch (error) {
+    console.log(error);
     next(error);
   }
 };
--- client/src/pages/Signin.jsx
+++ client/src/pages/Signin.jsx
@@ -1,7 +1,75 @@
-import React from "react";
+import axios from "axios";
+import React, { useState } from "react";
+import { Link } from "react-router-dom";
+import toast from "react-hot-toast";
+import { useNavigate } from "react-router-dom";
 
-const Signin = () => {
-  return <div>Signin</div>;
+const SignUp = () => {
+  const [formData, setFormdata] = useState({});
+  const [loading, setLoading] = useState(false);
+  const navigate = useNavigate();
+
+  const handleChange = (e) => {
+    setFormdata({ ...formData, [e.target.id]: e.target.value });
+  };
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    try {
+      setLoading(true);
+      const res = await axios.post(
+        "http://localhost:3000/api/auth/signin",
+        formData
+      );
+      console.log(res);
+      if (res.data.success === true) {
+        toast.success(res.data.message);
+        setLoading(false);
+        navigate("/");
+      }
+      if (res.data.success === false) {
+        setLoading(false);
+        toast.error(res.data.message);
+      }
+    } catch (error) {
+      setLoading(false);
+      toast.error("Something went wrong !!");
+    }
+  };
+
+  return (
+    <div className="max-w-lg mx-auto flex flex-col">
+      <h1 className="text-4xl text-center font-semibold my-8">Sign In</h1>
+      <form className="flex flex-col gap-4 mt-4 p-4" onSubmit={handleSubmit}>
+        <input
+          type="email"
+          placeholder="Email"
+          id="email"
+          className="bg-slate-100 p-2 rounded-lg"
+          onChange={handleChange}
+        />
+        <input
+          type="password"
+          placeholder="Password"
+          id="password"
+          className="bg-slate-100 p-2 rounded-lg"
+          onChange={handleChange}
+        />
+        <button
+          disabled={loading}
+          className="bg-blue-800 text-white rounded-lg mt-8 p-2 uppercase hover:opacity-95 disabled:opacity-80"
+        >
+          {loading ? "loading...." : "Sign in"}
+        </button>
+      </form>
+      <div className="flex p-4 gap-2">
+        <p>Don't Have an account?</p>
+        <Link to={"/signup"}>
+          <span className="text-blue-800">Sign Up</span>
+        </Link>
+      </div>
+    </div>
+  );
 };
 
-export default Signin;
+export default SignUp;
