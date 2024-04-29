0c2b96bd67c9fac952f83b03e4c7989e67a0e415
manishshukla00 <manishshuklacreative@outlook.com>
2024-04-29 13:34:55 +0530

    image upload on profile page added

--- client/src/pages/Profile.jsx
+++ client/src/pages/Profile.jsx
@@ -1,18 +1,55 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { useSelector } from "react-redux";
+import { useRef } from "react";
+import {
+  getDownloadURL,
+  getStorage,
+  ref,
+  uploadBytesResumable,
+} from "firebase/storage";
+import { app } from "../firebase";
 
 const Profile = () => {
   const { currentUser } = useSelector((state) => state.user);
-  console.log(currentUser.username);
+  const [image, setImage] = useState(undefined);
+  const [formData, setFormData] = useState({});
+  const fileRef = useRef(null);
+  console.log(formData);
+
+  useEffect(() => {
+    if (image) {
+      handleFileUpload(image);
+    }
+  }, [image]);
+
+  const handleFileUpload = (image) => {
+    const storage = getStorage(app);
+    const fileName = new Date().getTime() + image.name;
+    const storageRef = ref(storage, fileName);
+    const uploadTask = uploadBytesResumable(storageRef, image);
+    uploadTask.on("state_changed", () => {
+      getDownloadURL(uploadTask.snapshot.ref).then((downloadURL) =>
+        setFormData({ ...formData, profilePicture: downloadURL })
+      );
+    });
+  };
 
   return (
     <div className="max-w-lg mx-auto font-semibold text-center p-4">
       <h1 className="text-4xl my-4">Profile</h1>
       <form className="flex flex-col gap-8">
+        <input
+          type="file"
+          onClick={(e) => setImage(e.target.files[0])}
+          ref={fileRef}
+          accept="image/*"
+          hidden
+        />
         <img
           src={currentUser.profilePicture}
           alt="profile"
           className="w-20 h-20 self-center cursor-pointer rounded-full object-cover my-4"
+          onClick={() => fileRef.current.click()}
         />
         <input
           defaultValue={currentUser.username}
